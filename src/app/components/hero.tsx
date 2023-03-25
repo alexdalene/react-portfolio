66017c5a23dc90cb188b66c48a33d8718b6a60c8
Alex <111867370+alexdalene@users.noreply.github.com>
2023-03-25 16:29:11 +0100

    added animations and fixed some spacing and sizes

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,10 +1,13 @@
+'use client'
 import styles from './hero.module.scss'
+import { motion } from 'framer-motion'
 
 export default function Hero() {
     return (
-        <section className={styles.hero} >
-            <h1 className={styles.title} >Hello <br /> I&apos;m&nbsp;Alex.</h1>
-            <p className={styles.paragraph}>I am a <em>frontend-developer</em> with lots of <em>graphic design</em> experience.</p>
-        </section>
+        <motion.section className={styles.hero}>
+            <motion.h1 className={styles.title} initial={{ opacity: 0}} animate={{ opacity: 1}} >Hello <br /> I&apos;m&nbsp;Alex.</motion.h1>
+            <motion.hr className={styles.line} initial={{ width: 0, opacity: 0}} animate={{ width: 205, opacity: 1 }} transition={{ delay: 0.2}}/>
+            <motion.p className={styles.paragraph} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.5 }}>I am a <em>frontend-developer</em> with lots of <em>graphic design</em> experience.</motion.p>
+        </motion.section>
     )
 }
--- src/app/components/section.tsx
+++ src/app/components/section.tsx
@@ -1,15 +1,27 @@
+'use client'
 import styles from './section.module.scss'
 import Image from 'next/image'
+import { motion } from 'framer-motion'
 
 export default function Section() {
     return (
         <section className={styles.section}>
-            <div className={styles.templates}>
+            <motion.div className={styles.templates} 
+            initial={{ opacity: 0 }} 
+            whileInView={{ opacity: 1 }} 
+            viewport={{ once: true, margin: '-200px 0px' }}>
+                
                 <Image src="/svg/first-template.svg" alt="" width='200' height='200'/>
                 <Image src="/svg/second-template.svg" alt="" width='200' height='200'/>
                 <Image src="/svg/third-template.svg" alt="" width='200' height='200'/>
-            </div>
-            <h2 className={styles.title}>Take a look at <br /> my projects.</h2>
+            </motion.div>
+            <motion.h2 className={styles.title}
+            initial={{ opacity: 0 }} 
+            whileInView={{ opacity: 1 }} 
+            viewport={{ once: true, margin: '-200px 0px' }}>
+
+                Take a look at <br /> my projects.
+                </motion.h2>
         </section>
     )
 }
