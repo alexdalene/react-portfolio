1dd4a44a12373417ed65a717f0ec49296bbe21fa
Alex <111867370+alexdalene@users.noreply.github.com>
2023-03-25 17:04:09 +0100

    background was still buggy, read about a fix so added a new div that has background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import styles from './page.module.scss'
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div className={styles.background}></div>
       <Hero />
       <Section />
       <Project />
