45a9c5040cdc1df106dea37876d8b2e43c85c9c0
Alex Dalene <aledal51469@stud.noroff.no>
2023-03-26 13:01:18 +0200

    fixed a few things

--- src/app/components/project.tsx
+++ src/app/components/project.tsx
@@ -10,10 +10,6 @@ const container = {
       transition: {
         staggerChildren: 0.2
       },
-    onlyOnce: {
-        once: true,
-        margin: '-200px 0px'
-    }
     }
 }
 
@@ -29,12 +25,12 @@ const line = {
 
 function MakeProject({ type, title, description1, description2, url, gitLink, webLink}: { type: string, title: string, description1: string, description2: string, url: string, gitLink: string, webLink: string}) {
     return (
-    <motion.section className={styles.project} variants={ container } initial='hidden' whileInView='show' viewport={{ once: true, margin: '-200px 0px'}}>
+    <motion.section className={styles.project} variants={ container } initial='hidden' whileInView='show' viewport={{ once: true }}>
         <motion.h4 className={styles.type} variants={ children } >{type ? type : "Default type"}</motion.h4>
         <motion.h3 className={styles.title} variants={ children } >{title ? title : "Default title"}</motion.h3>
         <motion.p className={styles.description} variants={ children } >{description1 ? description1 : "Default description"}</motion.p>
         <motion.p className={styles.description} variants={ children } >{description2 ? description2 : "Default description"}</motion.p>
-        <motion.hr className={styles.line} variants={ line } />
+        {/* <motion.hr className={styles.line} variants={ line } /> */}
         <motion.div className={styles.imagesContainer} variants={ children } >
         <Image className={styles.image} src={url} alt="" width='300' height='300'/>
         </motion.div>
@@ -53,7 +49,7 @@ export default function Project() {
         title='Science&nbsp;Museum' 
         description1='Exam through school. We had to follow a few requirements, like text and content - but had full control over design.' 
         description2='I decided to go with a retro-futuristic look. I really wanted to challenge myself on this project.'
-        url='/images/science-museum.jpg'
+        url='/images/science-museum.png'
         gitLink='https://github.com/alexdalene/exam-des22'
         webLink='https://ad-science-museum.netlify.app/'
         ></MakeProject>
@@ -62,7 +58,7 @@ export default function Project() {
         title='Rainydays' 
         description1='This was our very first project using HTML and CSS. We later updated it to include REST API’s, making the products “real”.' 
         description2='Simplistic, modern and calming design - with a  focus on easy navigation.'
-        url='/images/rainydays.jpg'
+        url='/images/rainydays.png'
         gitLink='https://github.com/alexdalene/rainydays-cms'
         webLink='https://rainydays-cms-ad.netlify.app/'
         ></MakeProject>
@@ -71,7 +67,7 @@ export default function Project() {
         title='LotR&nbsp;API' 
         description1='First real project I used REST API’s with. Found a free Lord of the Rings API that had almost everything I wanted.' 
         description2='Went for a more modern-sleek look, inspired by material-design and GitHub.'
-        url='/images/lotr-api.jpg'
+        url='/images/lotr-api.png'
         gitLink='https://github.com/alexdalene/api-course-assignment'
         webLink='https://lotr-assignment.netlify.app/'
         ></MakeProject>
--- src/app/components/section.tsx
+++ src/app/components/section.tsx
@@ -3,24 +3,46 @@ import styles from './section.module.scss'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
 
+const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2
+      },
+    }
+}
+
+const children = {
+    hidden: { opacity: 0, scale: 0 },
+    show: { opacity: 1, scale: 1 }
+}
+
 export default function Section() {
     return (
         <section className={styles.section}>
-            <motion.div className={styles.templates} 
-            initial={{ opacity: 0 }} 
-            whileInView={{ opacity: 1 }} 
-            viewport={{ once: true, margin: '-200px 0px' }}>
-                
+            <motion.div className={styles.templates}
+            variants={ container }
+            initial='hidden' 
+            whileInView='show'
+            viewport={{ once: true }}>
+                <motion.div variants={ children }>
                 <Image src="/svg/first-template.svg" alt="" width='200' height='200'/>
+                </motion.div>
+                <motion.div variants={ children }>
                 <Image src="/svg/second-template.svg" alt="" width='200' height='200'/>
+                </motion.div>
+                <motion.div variants={ children }>
                 <Image src="/svg/third-template.svg" alt="" width='200' height='200'/>
+                </motion.div>
             </motion.div>
             <motion.h2 className={styles.title}
             initial={{ opacity: 0 }} 
-            whileInView={{ opacity: 1 }} 
-            viewport={{ once: true, margin: '-200px 0px' }}>
+            whileInView={{ opacity: 1 }}
+            transition={{ delay: 0.5 }} 
+            viewport={{ once: true }}>
 
-                Take a look at <br /> my projects.
+                Take a look at <br /> my projects below.
                 </motion.h2>
         </section>
     )
